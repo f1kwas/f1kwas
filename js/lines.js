e4652bcded3f107b4bf75e725ca4620d04d70f55
Marcin Kwasniewski <f1kwas@gmail.com>
2020-05-17 16:04:03 +0700

    Font resource and font loading added

--- js/lines.js
+++ js/lines.js
@@ -5,7 +5,8 @@ function main() {
   var lineHeight = 30;
   var lineColor = '#efefef';
   var textColor = '#c8c8c8';
-  var textFont = '75px ElementarzDwa';
+  var fontURL = '../fonts/Elementarz2.ttf';
+  var textFont = '75px schoolFont';
   var fileName = '../text.txt'
 
   var c = document.getElementById("myBoard");
