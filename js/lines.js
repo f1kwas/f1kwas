49ff3420a08426455d743749b1f04af222541747
Marcin Kwasniewski <f1kwas@gmail.com>
2020-05-17 16:21:19 +0700

    Remove unnecessary variables from js

--- js/lines.js
+++ js/lines.js
@@ -5,7 +5,6 @@ function main() {
   var lineHeight = 30;
   var lineColor = '#efefef';
   var textColor = '#c8c8c8';
-  var fontURL = '../fonts/Elementarz2.ttf';
   var textFont = '75px schoolFont';
   var fileName = '../text.txt'
 
