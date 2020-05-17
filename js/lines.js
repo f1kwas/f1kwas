edce47f5140c91c641331a2f4bff80f3ccce1354
Marcin Kwasniewski <f1kwas@gmail.com>
2020-05-17 11:28:14 +0700

    Fixed wrong lines.js version

--- js/lines.js
+++ js/lines.js
@@ -6,12 +6,14 @@ function main() {
   var lineColor = '#efefef';
   var textColor = '#c8c8c8';
   var textFont = '75px ElementarzDwa';
+  var fileName = '../text.txt'
 
   var c = document.getElementById("myBoard");
   c.width = pageWidth;
   c.height = pageHeight;
 
   drawLines(c, lineHeight, lineColor);
+  doText(fileName, c, lineHeight, textColor, textFont);
 }
 
 function drawLines(c, height, color) {
@@ -25,6 +27,23 @@ function drawLines(c, height, color) {
       ctx.lineTo(pageWidth, vertical);
       ctx.stroke();
     }
+  }
+}
+
+function doText(fileName, c, lineHeight, color, font) {
+  var promise = fetch(fileName)
+    .then(response => response.text())
+    .then(texts => {
+      texts = texts.split('\n');
+      printLines(c, texts, lineHeight, color, font);
+    });
+}
+
+function printLines(c, text, lineHeight, color, font) {
+  var textVerticalPosition = 0;
+  for (var i = 0; i < text.length; i++) {
+    textVerticalPosition = lineHeight * 4 * (i + 1) - lineHeight;
+    printText(c, text[i], textVerticalPosition, color, font);
   }
 }
 
